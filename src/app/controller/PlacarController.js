5fa3397008aa5286b50a903add362f21ce0925f8
Bruno de Souza Valeiro <bvaleiro85@hotmail.com>
2019-08-25 12:57:24 -0300

    placar realtime

--- src/app/controller/PlacarController.js
+++ src/app/controller/PlacarController.js
@@ -7,7 +7,9 @@ const auth = require('../midleware/auth');
 router.post('/criaPlacar',async(req,res)=>{
     //console.log(checkBid);
     const criaPlacar = await Placar.create(req.body);
-    return res.json({
+	const listaPlacar = await Placar.find({}).sort('data');
+   	req.io.emit('placar',listaPlacar);
+	return res.json({
                     success:true,
                     message:'usuario Cadastrado com SUCESSO',
                     });  
