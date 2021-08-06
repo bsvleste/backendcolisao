a5a6ebbef18c204c0b9ea4c1c459e3c3e5861e63
Bruno <bvaleiro@gmail.com>
2021-08-05 22:13:20 -0300

    melherando contagem do bid

--- src/app/controller/BidController.js
+++ src/app/controller/BidController.js
@@ -3,6 +3,15 @@ const Bid = require('../models/BidModel');
 module.exports = {
     async getBid(req, res) {
         const listaBid = await Bid.find({}).populate('usuario').sort('bid');
+        const checkBid = await Bid.aggregate([
+            {
+                $group: {
+                    _id: '$bid',
+                    count: { $sum: 1 },
+                },
+            },
+        ]);
+        req.io.emit('contagem', checkBid);
         return res.json(listaBid);
     },
     async addBid(req, res) {
@@ -27,6 +36,6 @@ module.exports = {
 
         req.io.emit('bid', listaBid);
 
-        return res.json(listaBid);
+        return res.json(checkBid);
     },
 };
