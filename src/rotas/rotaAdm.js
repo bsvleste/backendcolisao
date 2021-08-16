c07289c48349ddd6e3abc2773fc90fdd50e6934f
Bruno <bvaleiro@gmail.com>
2021-08-16 09:51:54 -0300

    post rota delete

--- src/rotas/rotaAdm.js
+++ src/rotas/rotaAdm.js
@@ -10,7 +10,7 @@ router.route('/criaPlacar').post(PlacarController.criarPlacar);
 router.route('/editar/:id').get(PlacarController.editar);
 router.route('/deleta/:id').post(PlacarController.deletaPlacar);
 router.route('/update').post(PlacarController.update);
-router.route('/deletaBid').delete(BidControler.delete);
+router.route('/deletaBid').post(BidControler.delete);
 router.route('/liberaBid').post(BidControler.liberaBid);
 router.route('/').get(BidControler.getStatus);
 module.exports = router;
